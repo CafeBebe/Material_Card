0b380d19d54d4efbd6526938410b8105ff7f6948
Kondratiev Kirillius <ko.ki.ig1991@gmail.com>
2024-05-31 12:33:25 +0300

    created map material

--- app/src/main/java/com/example/materialcard/MainActivity.kt
+++ app/src/main/java/com/example/materialcard/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.materialcard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.materialcard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.containedIconButton?.setOnClickListener(View.OnClickListener {
+            binding?.badBoys?.text = "Hello"
+        }
+        )
+        binding?.card?.setOnClickListener {
+            binding?.card?.isChecked = !binding?.card?.isChecked!!
+            true
+        }
+
     }
 }
